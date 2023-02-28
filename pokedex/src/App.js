bce2cfe5ed69dddd7c730194629e3cb857fdbdb5
LuisFranco12 <luisf3525@gmail.com>
2023-02-28 16:02:02 -0500

    called fetch function with useEffect hook

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-
+import { FetchPokemonList } from "./api/FetchPokemonList"
 function App() {
   const [pokemonList, setPokemonList] = useState(null)
   const [loading, setLoading] = useState(false)
   const [currentPage] = useState(1)
   const [pokemonPerPage, setPokemonPerPage] = useState(12)
   const [input, setInput] = useState('')
-  
+
+  useEffect(() => {
+    setLoading(true)
+    FetchPokemonList(setPokemonList, 1, 151)
+    setLoading(false)
+  }, [])
+
   return (
     <div className="App">
 
