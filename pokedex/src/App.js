06a4b6e46a008c62d2fab0173bd7bddb386bd755
LuisFranco12 <luisf3525@gmail.com>
2023-02-28 15:58:42 -0500

    created hooks

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [currentPage] = useState(1)
+  const [pokemonPerPage, setPokemonPerPage] = useState(12)
+  const [input, setInput] = useState('')
+  
   return (
     <div className="App">
 
