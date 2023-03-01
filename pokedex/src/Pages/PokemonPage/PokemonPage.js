9f23b776a317ea9348075a06fbdc0094d5223c06
LuisFranco12 <luisf3525@gmail.com>
2023-03-01 16:59:15 -0500

    added more info to pokemon page

--- pokedex/src/Pages/PokemonPage/PokemonPage.js
+++ pokedex/src/Pages/PokemonPage/PokemonPage.js
@@ -7,6 +7,7 @@ const PokemonPage = () => {
     return ( 
         <PokemonPageContainer>
             <div className="pokemon-page">
+
                 <div className="left-side">
                     <img src={state.sprites.other.home.front_default} alt={PokemonPage.name}/>
                     <h1>STATS</h1>
@@ -14,17 +15,48 @@ const PokemonPage = () => {
                         state.stats.map(item => <PokemonPageStats item={item}/>)
                     }
                 </div>
+
                 <div className="right-side">
                     <p id="description"></p>
+
                     <div className="pokemon-info">
-                        <div className="left-info"></div>
-                        <div className="right-info"></div>
-                    </div>
-                    <div className="types">
-                        <p>Type(s)</p>
-                        <div className="types"></div>
+
+                        <div className="left-info">
+                            <div className="height">
+                                <div>Height</div>
+                                <div>{ (state.height / 3.048).toFixed(2) } Feet</div>
+                            </div>
+                            <div className="weight">Weight</div>
+                            <div>{ (state.weight / 4.536).toFixed(1) } pounds</div>
+                        </div>
+
+                        <div className="right-info">
+                            <div className="types-container">
+                                <div>Type(s)</div>
+                                <div className="types">
+                                    {
+                                        state.types.map(item => (
+                                            <div>{item.type.name}</div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+
+                            <div className="abilities-container">
+                                <div>Abilities</div>
+                                <div className="abilities">
+                                    {
+                                        state.abilities.map(item => (
+                                            <div>{item.ability.name}</div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
+
             </div>
         </PokemonPageContainer>
      );
